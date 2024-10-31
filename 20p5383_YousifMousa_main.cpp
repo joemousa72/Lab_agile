fea15dca709f045cefbaeb1333bfa4daa26869bf
joemousa72 <yousifmousa72@gmail.com>
2024-10-31 20:11:19 +0300

    initial commit: add sum function

---
+++ 20p5383_YousifMousa_main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int sum(int x, int y) {
+	return x + y;
+}
+int main() {
+	int num1, int num2;
+	cout << "Please enter two integers:";
+	cin >> num1 >> num2;
+	cout << "The sum is:" << sum(num1 + num2)
+		return 0;
+}
